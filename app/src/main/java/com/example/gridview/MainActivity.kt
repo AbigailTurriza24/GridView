fd606ffa43819515f5f9b6641ee0b2f15caebbdf
Abigail Turriza <abigailturriza3@gmail.com>
2022-03-06 22:18:02 -0500

    Commit en Main Activity

--- app/src/main/java/com/example/gridview/MainActivity.kt
+++ app/src/main/java/com/example/gridview/MainActivity.kt
@@ -6,7 +6,7 @@ import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.GridView
 import android.widget.Toast
-
+//Main Activity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
